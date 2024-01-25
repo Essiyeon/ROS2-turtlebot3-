fa4b4fa619b9c536eb018406206528f6eb0c4037
Essiyeon <aeiou0302@icloud.com>
2024-01-25 13:35:21 +0900

    2024-01-25 re Commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# ROS2_turtlebot3
+# gongju-university-ros2
+
+---
+2024_01_22
+---
+
+- ros2 개요 설명
+- vmwarer 설치
+- ubuntu 22.04.5 
+- asdf
---
+++ arithmetic/arithmetic/arg.py
@@ -0,0 +1,11 @@
+import rclpy
+from rclpy.node import Node
+from rclpy.qos import QoSProfile
+from my_interface.msg import ArithmeticArgument
+from rcl_interfaces.msg import SetParametersResult
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
